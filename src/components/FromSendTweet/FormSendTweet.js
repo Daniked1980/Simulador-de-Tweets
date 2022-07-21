301b9af4b515db4e4f5aa3aa374bd5d21fc6b800
Danjar27 <losteven2018@outlook.com>
2022-07-20 21:58:18 -0500

    adjust base

--- src/components/FromSendTweet/FormSendTweet.js
+++ src/components/FromSendTweet/FormSendTweet.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { FormControl, FormGroup, TextField, Button } from "@mui/material";
+import React,{useState} from "react";
+import { FormControl, FormGroup, TextField, Button, useStepContext  } from "@mui/material";
 import "./FormSendTweet.scss";
-export default function FormSendTweet(props) {
-    const { sendTweet } = props;
-    const [formVaule, setFormValue] = useState({
-        name: "",
-        tweet: ""
+export default function FormSendTweet(props){
+    const {sendTweet}=props;
+    const [formVaule, setFormValue]=useState({
+        name:"",
+        tweet:""
     });
-
+    
     const onFormChange = event => {
         setFormValue({
             ...formVaule,
@@ -16,39 +16,39 @@ export default function FormSendTweet(props) {
     }
 
 
-    return (
+    return(
         <div className="formsendtweet">
-            <h2 className="form-send-tweet_title">Enviar Tweet</h2>
-            <form
-                className="form-send-tweet_form"
-                onSubmit={(event) => sendTweet(event, formVaule)}
-                onChange={onFormChange}>
-                <FormControl>
-                    <FormGroup>
-                        <TextField
-                            className="form-send-tweet_form-name"
-                            type="text"
-                            name="name"
-                            placeholder="nombre de usuario"
-                            margin="normal" />
-                    </FormGroup>
-                    <FormGroup>
-                        <TextField className="form-send-tweet_form-textarea"
-                            name="tweet"
-                            multiline
-                            rows="6"
-                            placeholder="Escribe tu tweet..."
-                            margin="normal" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Button type="Submit">
-                            Enviar Tweet
-                        </Button>
-                    </FormGroup>
-
-                </FormControl>
-
-            </form>
+        <h2 className="form-send-tweet_title">Enviar Tweet</h2>
+        <form 
+        className="form-send-tweet_form" 
+        onSubmit={event=>sendTweet (event, formVaule)} 
+        onChange={onFormChange}>
+            <FormControl>
+                <FormGroup>
+                    <TextField 
+                    className="form-send-tweet_form-name"
+                    type="text"
+                    name="name"
+                    placeholder="nombre de usuario"
+                    margin="normal"/>
+                </FormGroup>
+                <FormGroup>
+                    <TextField className="form-send-tweet_form-textarea"
+                    name="tweet"
+                    multiline
+                    rows="6"
+                    placeholder="Escribe tu tweet..."
+                    margin="normal"/>
+                </FormGroup>
+                <FormGroup>
+                    <Button type="Submit">
+                        Enviar Tweet
+                    </Button>
+                </FormGroup>
+                
+            </FormControl>
+            
+        </form>
         </div>
     );
 }
